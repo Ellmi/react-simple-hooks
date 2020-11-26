65b412b3f10eb7ba3a8d2291d43054464e90a9e0
Yanmin <ymxing@thoughtworks.com>
2020-11-26 22:07:10 +0800

    add page navigator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+    const [page, setPage] = useState(1);
+
     return (
         <div className="App">
+            <div>
+                <button onClick={() => {
+                    if (page > 1) setPage(page - 1)
+                }}>
+                    PREV
+                </button>
+                <p>Current Page: {page}</p>
+                <button onClick={() => {
+                    if (page < 2) setPage(page + 1)
+                }}>
+                    NEXT
+                </button>
+            </div>
         </div>
     );
 }
