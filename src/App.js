221039d1c59fe0896c613474d45e040a29bcd9be
Yanmin <ymxing@thoughtworks.com>
2020-11-26 23:20:14 +0800

    add loading

--- src/App.js
+++ src/App.js
@@ -5,45 +5,51 @@ function App() {
     const [page, setPage] = useState(1);
     const [maxPage, setMaxPage] = useState(1);
     const [items, setItems] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         fetch("https://reqres.in/api/users?page=1")
             .then(res => res.json())
             .then(
                 (result) => {
+                    setIsLoaded(true)
                     setItems(result.data);
                     setMaxPage(result.total_pages);
                 }
             )
     }, [])
-    return (
-        <div className="App">
-            <div>
-                <button onClick={() => {
-                    if (page > 1) setPage(page - 1)
-                }}>
-                    PREV
-                </button>
-                <p>Current Page: {page}</p>
-                <button onClick={() => {
-                    if (page < maxPage) setPage(page + 1)
-                }}>
-                    NEXT
-                </button>
+    if (!isLoaded) {
+        return <div className="App">Loading...</div>;
+    } else {
+        return (
+            <div className="App">
+                <div>
+                    <button onClick={() => {
+                        if (page > 1) setPage(page - 1)
+                    }}>
+                        PREV
+                    </button>
+                    <p>Current Page: {page}</p>
+                    <button onClick={() => {
+                        if (page < maxPage) setPage(page + 1)
+                    }}>
+                        NEXT
+                    </button>
+                </div>
+                <div>
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                {item.first_name} {item.last_name}
+                                {item.email}
+                                <img alt="avatar" src={item.avatar}/>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-            <div>
-                <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-                            {item.first_name} {item.last_name}
-                            {item.email}
-                            <img src={item.avatar}/>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
