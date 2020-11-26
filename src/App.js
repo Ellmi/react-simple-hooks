c5c0ffa9425a4dc7d151ca852b03abff78d2a9a8
Yanmin <ymxing@thoughtworks.com>
2020-11-26 23:13:08 +0800

    fetch api and list info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
     const [page, setPage] = useState(1);
+    const [maxPage, setMaxPage] = useState(1);
+    const [items, setItems] = useState([]);
 
+    useEffect(() => {
+        fetch("https://reqres.in/api/users?page=1")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setItems(result.data);
+                    setMaxPage(result.total_pages);
+                }
+            )
+    }, [])
     return (
         <div className="App">
             <div>
@@ -14,10 +26,21 @@ function App() {
                 </button>
                 <p>Current Page: {page}</p>
                 <button onClick={() => {
-                    if (page < 2) setPage(page + 1)
+                    if (page < maxPage) setPage(page + 1)
                 }}>
                     NEXT
                 </button>
+            </div>
+            <div>
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            {item.first_name} {item.last_name}
+                            {item.email}
+                            <img src={item.avatar}/>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
